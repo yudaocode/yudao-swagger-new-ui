8aabc377f2173cd5b2648e0e816f90a98d58a758
cherishsince <cherishsince@aliyun.com>
2026-03-02 17:15:13 +0800

    【新增】请求proxy代理，避免跨域问题（只针对 test）

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -10,6 +10,10 @@ export default defineConfig({
         target: 'http://127.0.0.1:6618',
         changeOrigin: true,
       },
+      '/api': {
+        target: 'http://127.0.0.1:6618',
+        changeOrigin: true,
+      },
     },
   },
 })
