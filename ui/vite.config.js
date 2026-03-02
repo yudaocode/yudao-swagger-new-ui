8bc3c8a32572af7ef47de63da3c6847ec2571906
cherishsince <cherishsince@aliyun.com>
2026-03-02 15:25:26 +0800

    【新增】swagger api 代理，避免跨域

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/v3': {
+        target: 'http://127.0.0.1:6618',
+        changeOrigin: true,
+      },
+    },
+  },
 })
