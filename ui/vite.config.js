a8adaf5f29ac97a14f766be1ac9a4484e72c55ef
cherishsince <cherishsince@aliyun.com>
2026-03-02 22:24:52 +0800

    【优化】支持 build:java

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/v3': {
-        target: 'http://127.0.0.1:6618',
+        target: 'http://127.0.0.1:8080/admin',
         changeOrigin: true,
       },
       '/api': {
-        target: 'http://127.0.0.1:6618',
+        target: 'http://127.0.0.1:8080/admin',
         changeOrigin: true,
       },
     },
